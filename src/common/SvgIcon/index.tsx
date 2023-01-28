a5eacd6ead6856d5d49f809887c63673b4d171a1
Ruben Oosterink <jb.ict.info@gmail.com>
2023-01-28 20:28:02 +0100

    update package

--- src/common/SvgIcon/index.tsx
+++ src/common/SvgIcon/index.tsx
@@ -1,5 +1,5 @@
 import { SvgIconProps } from "../types";
 
 export const SvgIcon = ({ src, width, height, className }: SvgIconProps) => (
-  <img loading="eager" src={`/img/svg/${src}`} alt={src} width={width} height={height} className={className} />
+  <img loading="eager" src={process.env.PUBLIC_URL + `/img/svg/${src}`} alt={src} width={width} height={height} className={className} />
 );
