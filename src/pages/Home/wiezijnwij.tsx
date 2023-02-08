07578a901a0d7536c5afd22c51016c412149c357
Ruben Oosterink <jb.ict.info@gmail.com>
2023-02-08 17:21:19 +0100

    Styling update

--- src/pages/Home/wiezijnwij.tsx
+++ src/pages/Home/wiezijnwij.tsx
@@ -1,19 +1,25 @@
-import { Link } from "react-router-dom";
-import { SvgIcon } from "../../common/SvgIcon";
-import { H1WZW } from "../../components/results/styles";
+import {Link} from "react-router-dom";
+import {SvgIcon} from "../../common/SvgIcon";
+import {H1WZW} from "../../components/results/styles";
 
 const WieZijnWij = () => {
-  return (
-    <div className="text-center">
-      <img loading='lazy' src="/img/wiezijnwij.jpg" style={{ height: '100vh', width: '100%', objectFit: 'cover',}} alt="wiezijnwij"></img>
-      <Link onClick={() => window.fullpage_api.moveTo(1)} to="/ditzijnwij" className={"d-flex flex-column align-items-start"} style={{ position: 'absolute' , top: '50%' , left: '50%', transform: 'translate(-50%, -50%)'}} >
-        <H1WZW>WIE</H1WZW>
-        <H1WZW>ZIJN</H1WZW>
-        <H1WZW>WIJ</H1WZW>
-        <SvgIcon className={"d-flex align-self-center pt-2 pt-lg-1"} src="wiezijnwij.svg" width="100%" height="100%"/>
-      </Link>
-    </div>            
-  );
+    return (
+        <div className="text-center">
+            <img className={"d-none d-lg-block"} loading='lazy' src="/img/teamfoto_desktop.jpg"
+                 style={{height: '100vh', width: '100%', objectFit: 'cover',}} alt="wiezijnwij"></img>
+            <img className={"d-block d-lg-none"} loading='lazy' src="/img/teamfoto_mobiel.jpg"
+                 style={{height: '100vh', width: '100%', objectFit: 'cover',}} alt="wiezijnwij"></img>
+            <Link onClick={() => window.fullpage_api.moveTo(1)} to="/ditzijnwij"
+                  className={"d-flex flex-column align-items-start"}
+                  style={{position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)'}}>
+                <H1WZW>WIE</H1WZW>
+                <H1WZW>ZIJN</H1WZW>
+                <H1WZW>WIJ</H1WZW>
+                <SvgIcon className={"d-flex align-self-center pt-2 pt-lg-1"} src="wiezijnwij.svg" width="100%"
+                         height="100%"/>
+            </Link>
+        </div>
+    );
 };
 
 export default WieZijnWij;
