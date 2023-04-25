8af431691ae8f5ec4f1f11c5d41152f3c034f0ba
Ruben Oosterink <jb.ict.info@gmail.com>
2023-04-25 08:59:57 +0200

    add pretier

--- src/serviceWorkerRegistration.ts
+++ src/serviceWorkerRegistration.ts
@@ -47,7 +47,7 @@ export function register(config?: Config) {
         // service worker/PWA documentation.
         navigator.serviceWorker.ready.then(() => {
           console.log(
-            "This web app is being served cache-first by a service " + "worker"
+            "This web app is being served cache-first by a service worker"
           );
         });
       } else {
