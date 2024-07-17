91051750dead77dad5b4323dbe138bd4eeaae8c3
Ruben Oosterink <jb.ict.info@gmail.com>
2024-07-17 15:51:41 +0200

    Updates

---
+++ assets/styles-D9bTuqWy.js
@@ -0,0 +1,35 @@
+import{p as e}from"./index-ACZDTmo_.js";const o=e("h1")`
+  font-size: 80px;
+  font-family: Neue Haas Black;
+  color: #1b3888 !important;
+
+  @media screen and (min-width: 992px) {
+    font-size: 145px;
+  }
+`,a=e("h1")`
+  font-size: 55px;
+  font-family: Neue Haas Bold;
+  color: #e4e4e4;
+
+  @media screen and (min-width: 992px) {
+    font-size: 130px;
+  }
+`,t=e("h1")`
+  font-size: 55px;
+  font-family: Neue Haas Black;
+  color: #e4e4e4;
+
+  @media screen and (min-width: 992px) {
+    font-size: 130px;
+  }
+`,i=e("p")`
+  font-size: 22px;
+  font-family: Neue Haas Medium;
+  color: #1b3888;
+  text-align: center;
+  margin-bottom: 0;
+
+  @media screen and (min-width: 992px) {
+    font-size: 35px;
+  }
+`;export{o as H,i as P,a,t as b};
