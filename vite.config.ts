5e763a44b209b2cee87e34a6b7371c595a362aa7
Ruben Oosterink <ruben.oosterink@edsn.nl>
2026-06-28 12:32:31 +0200

    fix service worker 2

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import svgr from "vite-plugin-svgr";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,5 +12,34 @@ export default defineConfig({
       "~bootstrap": path.resolve(__dirname, "node_modules/bootstrap"),
     },
   },
-  plugins: [react(), svgr()],
+  plugins: [
+    react(),
+    svgr(),
+    VitePWA({
+      srcDir: "src",
+      filename: "service-worker.ts",
+      strategies: "injectManifest",
+      injectManifest: {
+        swDest: "dist/service-worker.js",
+      },
+      manifest: {
+        name: "Stadslab Zeist",
+        short_name: "Stadslab",
+        description: "Stadslab Zeist",
+        theme_color: "#000000",
+        icons: [
+          {
+            src: "img/logo.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "img/logo.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
 });
