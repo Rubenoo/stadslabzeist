3df6f3af5e49e4b87a1e40eaf77297a3c97f4241
Ruben Oosterink <ruben.oosterink@edsn.nl>
2026-06-28 12:14:00 +0200

    Updates

--- assets/index-D6Vlap8r.js
+++ assets/index-D6Vlap8r.js
@@ -1,4 +1,4 @@
-import{p as t,j as i,S as e}from"./index-6qmlfaPw.js";const r=t("div")`
+import{p as t,j as i,S as e}from"./index-pJUivPDK.js";const r=t("div")`
   padding: 10px;
   position: fixed;
   right: 30px;
